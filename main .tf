1f9088653a8fb08e8c4149d743e4289b4e1a62a2
github9321 <sandhyasantu7059@gmail.com>
2026-02-11 20:54:28 +0530

    main.tf & provider.tf

---
+++ main .tf
@@ -0,0 +1,17 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "multi-instance"
+
+  instance_type = "t3.micro"
+  key_name      = "san-26"
+  monitoring    = true
+  subnet_id     = "subnet-0ab2056f9db101057"
+  vpc_security_group_ids = [ "sg-080b9b77149ca75e6" ]
+  count = 3
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
